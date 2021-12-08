78eee052d1cbdaccb6bd2a06592c1c2adb86b697
Dominik Grybos <dominik.grybos@gmail.com>
2021-12-08 22:57:56 +0100

    Add tasty with simple unit tests

---
+++ test/Test.hs
@@ -0,0 +1,20 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Data.List
+import Data.Ord
+
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+
+unitTests = testGroup "Unit tests"
+  [ testCase "List comparison (different length)" $
+      [1, 2, 3] `compare` [1,2] @?= GT
+  , testCase "List comparison (same length)" $
+      [1, 2, 3] `compare` [1,2,2] @?= GT
+  ]
